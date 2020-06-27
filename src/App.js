20c4ea67159c0e5776d6f712ef6f02f2861b488d
Erick-SN <ericksn0707@gmail.com>
2020-06-26 23:22:34 -0500

    BreakingBad Phrase Generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import styled from '@emotion/styled';
+import Phrase from './Components/Phrase';
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  background: -webkit-linear-gradient(
+    top left,
+    #007d35 0%,
+    #007d35 40%,
+    #0f574e 100%
+  );
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 0.5rem;
+  font-size: 2rem;
+  border: none;
+  transition: background-size 0.8s ease;
+  :hover {
+    cursor: pointer;
+    background-size: 400px;
+  }
+`;
 
 function App() {
+  const [phrase, setPhrase] = useState({});
+  const getPhrase = async () => {
+    const response = await fetch(
+      `https://breaking-bad-quotes.herokuapp.com/v1/quotes`,
+    ).then((res) => res.json());
+    setPhrase(response[0]);
+  };
+  useEffect(() => {
+    getPhrase();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <Phrase phrase={phrase} />
+        <Button onClick={() => getPhrase()}>Get Phrase</Button>
+      </Container>
+    </>
   );
 }
 
